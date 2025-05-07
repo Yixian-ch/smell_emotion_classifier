de145166b2e92f30350ecc73d29caa3d138cc5fd
Joce <jocelyn.zaruma@proton.me>
2025-05-07 15:23:31 +0200

    ajout pour le classifier Naives Bayes

---
+++ classifier_nb.py
@@ -0,0 +1,37 @@
+import pandas as pd
+from sklearn.feature_extraction.text import TfidfVectorizer
+from spacy.lang.fr.stop_words import STOP_WORDS as fr_stop
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
+
+df_surprise = pd.read_csv("surprise.csv")
+df_fear = pd.read_csv("fear.csv")
+df = pd.concat([df_surprise, df_fear], ignore_index=True)
+nb = MultinomialNB()
+
+X = df['text']
+y = df['emotion']
+
+vectorizer = TfidfVectorizer(stop_words=list(fr_stop))
+X_vectorized = vectorizer.fit_transform(X)
+X_train, X_test, y_train, y_test = train_test_split(
+    X_vectorized, y, test_size=0.3, random_state=25
+)
+trained_model = nb.fit(X_train, y_train)
+predictions = trained_model.predict(X_test)
+report = classification_report(y_test, predictions)
+print("Naive Bayes Classification Report:\n",report )
+
+feature_names = vectorizer.get_feature_names_out()
+class_labels = nb.classes_
+log_probs = nb.feature_log_prob_  
+
+top_n = 20
+for i, label in enumerate(class_labels):
+    print(f"\nTop {top_n} words for class '{label}':")
+    top_features = log_probs[i].argsort()[::-1][:top_n]
+    for idx in top_features:
+        word = feature_names[idx]
+        weight = log_probs[i][idx]
+        print(f"{word}\t{weight:.3f}")
